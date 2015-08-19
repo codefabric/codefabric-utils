076f40a527001b4c40950582190573848664890c
David Else <dave@daveelse.com>
2015-08-19 21:50:21 +0100

    Adding appveyor CI build script

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,5 @@
 var gulp = require("gulp"),
-    run = require("gulp-run"),
-    exec = require("gulp-exec");
+    run = require("gulp-run");
 	
 var project = {
 	target: 'Debug',
@@ -11,37 +10,13 @@ var project = {
 gulp.task('default', ['build']);
 
 gulp.task('build', function () {
-
-var options = {
-  continueOnError: false, // default = false, true means don't emit error event 
-  pipeStdout: false, // default = false, true means stdout is written to file.contents
-};
-var reportOptions = {
-  err: true, // default = true, false means don't write err 
-  stderr: true, // default = true, false means don't write stderr 
-  stdout: true // default = true, false means don't write stdout 
-}
   gulp.src('./**/project.json')
   			 .pipe(run('dnu build <%= file.path %> --quiet'));
-  			 // .pipe(exec('dnu build <%= file.path %> --quiet', options))
-  			 // .pipe(exec.reporter(reportOptions));
 });
 
 gulp.task('test', function() {
-
-var options = {
-  continueOnError: false, // default = false, true means don't emit error event 
-  pipeStdout: false, // default = false, true means stdout is written to file.contents
-};
-var reportOptions = {
-  err: true, // default = true, false means don't write err 
-  stderr: true, // default = true, false means don't write stderr 
-  stdout: true // default = true, false means don't write stdout 
-}
   gulp.src('./**.tests/project.json')
   			 .pipe(run('dnx <%= file.path %> test'));
-             // .pipe(exec('dnx <%= file.path %> test', options))
-  			     // .pipe(exec.reporter(reportOptions));
 });
 
 gulp.task('watch', function() {
